9c2d3e6743b7748ba4e7cf495fe7140d3697a0ac
hbzxc <hjkb808@gmail.com>
2024-03-03 16:15:23 -1000

    updated footer

--- react/firemap/src/footer.js
+++ react/firemap/src/footer.js
@@ -4,9 +4,13 @@ import Typography from "@mui/material/Typography";
 const Footer = () => {
   return (
     <footer style={{ backgroundColor: '#1D6069', padding: '10px', textAlign: 'center', position: 'fixed', bottom: '0', width: '100%' }}>
-    <Typography variant="body2" style={{ color: 'white' }}>
-        &copy; {new Date().getFullYear()} Henry Blazier | <a href="https://hbzxc.github.io/" target="_blank" style={{ color: 'white' }}>https://hbzxc.github.io/</a>
-      </Typography>
+<Typography variant="body2" style={{ color: 'white' }}>
+  &copy; {new Date().getFullYear()} | This project was funded by
+  <a href="https://www.fs.usda.gov/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: 'inherit' }}> USDA Forest Service </a> and implemented by 
+  <a href="https://www.hawaiiwildfire.org/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: 'inherit' }}> Hawaii Wildfire Management Organization </a> in partnership with the 
+  <a href="https://thehetf.us/ipif/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'underline', color: 'inherit' }}> Institute of Pacific Islands Forestry </a>
+</Typography>
+
     </footer>
   );
 };
